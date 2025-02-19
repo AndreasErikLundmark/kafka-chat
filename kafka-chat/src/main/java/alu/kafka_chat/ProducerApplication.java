55d3a5da16a697b4a912c1dbd72e4b2f1a8edd6a
Andreas Lundmark <andreas.erik.lundmark@gmail.com>
2025-02-19 22:44:30 +0100

    producer basic

---
+++ kafka-chat/src/main/java/alu/kafka_chat/ProducerApplication.java
@@ -0,0 +1,33 @@
+package alu.kafka_chat;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaTemplate;
+
+@SpringBootApplication
+public class ProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducerApplication.class, args);
+	}
+
+	@Bean
+	public NewTopic topic() {
+		return TopicBuilder.name("test-topic")
+				.partitions(10)
+				.replicas(1)
+				.build();
+	}
+
+	@Bean
+	public ApplicationRunner runner(KafkaTemplate<String, String> template) {
+		return args -> {
+			template.send("test-topic", "starting Producer OK");
+		};
+	}
+
+}
