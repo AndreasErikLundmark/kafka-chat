a853c90d03b3f4e1297b3f9328d722c37e934e25
Andreas Lundmark <andreas.erik.lundmark@gmail.com>
2025-02-19 14:49:25 +0100

    producer controller

--- kafka-chat/src/main/java/alu/kafka_chat/controller/ChatController.java
+++ kafka-chat/src/main/java/alu/kafka_chat/controller/ChatController.java
@@ -1,16 +1,37 @@
 package alu.kafka_chat.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
 
+
+/**
+ * Producer
+ */
 @RequestMapping("/chat")
 @RestController
 @CrossOrigin
 public class ChatController {
 
-    @PostMapping
-    public String sendMsg(@RequestParam String topic, @RequestParam String msg) {
+    private final KafkaTemplate<String, String> template;
+//    private final ObjectMapper mapper;
+    public ChatController(KafkaTemplate<String, String> template) {
+        this.template = template;
+    }
 
-        return "Sent message: " + msg;
+//    public ChatController(KafkaTemplate<String, String> template) {
+//        this.template = template;
+//        mapper = new ObjectMapper();
+//    }
+
+    @PostMapping
+    public ResponseEntity<String> sendMsg(@RequestParam String topic, @RequestParam String msg) {
+        template.send(topic, msg);
+
+        return ResponseEntity.status(HttpStatus.OK).body("Sent message: " + msg);
+
     }
 
 }
